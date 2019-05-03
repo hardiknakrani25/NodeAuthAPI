a2dcd955dea89c4f17ffe4b1349ab6036959fe0e
Hardik <hdnakarani@gmail.com>
2019-05-03 17:49:13 +0530

    get request added

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const express=require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Welcome to the API"
+  });
+});
+
+app.listen(5000, () => console.log("Server Started On Port 5000"));
